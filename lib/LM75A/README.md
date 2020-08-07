d62794ead332af7bc3648d58d429e758813aa7f5
nicholas@orangecharger.com <nicholas@orangecharger.com>
2020-08-07 14:03:11 -0700

    Things kinda work, Temp works on LM75A, MCP39F is still not communicating nicly

---
+++ lib/LM75A/README.md
@@ -0,0 +1,18 @@
+# I2C LM75A Temperature Sensor
+
+## author
+Quentin Comte-Gaz
+
+## license
+MIT License
+
+## source
+https://github.com/QuentinCG/Arduino-LM75A-Temperature-Sensor-Library
+
+## Usage
+See example
+
+## LICENSE
+Copyright 2018 Quentin Comte-Gaz
+
+Licensed under the MIT license
--- src/I2C-MCP32F521.cpp
+++ src/I2C-MCP32F521.cpp
@@ -8,16 +8,26 @@
 //  * Project I2C-MCP32F521
 //  * Description: 
 //  * Author: Nicholas 
-//  * Date: July 16th 2020 
+//  * Date: Aug 6th 2020 
 //  */
 
 #include "Wire.h"
 #include "particle.h"
+#include <LM75A.h>
 
+int readMCP32f521(int addressHigh, int addressLow, int numBytesToRead, uint8_t *byteArray, int byteArraySize);
 void wireErrors(uint8_t i2c_bus_Status);
+void LM75A_TEMP_READING();
 void setup();
 void loop();
-#line 11 "/Users/nicholas/Documents/Particle/I2C-MCP32F521/src/I2C-MCP32F521.ino"
+#line 12 "/Users/nicholas/Documents/Particle/I2C-MCP32F521/src/I2C-MCP32F521.ino"
+SYSTEM_MODE(SEMI_AUTOMATIC)
+
+int tinkerDigitalWrite(String command);
+
+// Create I2C LM75A instance
+LM75A lm75a_sensor(false, false, false); //A1, A2, A3 LM75A pin state for I2C address 
+
 typedef struct MCP39F521_Data {
 	uint16_t systemStatus;
 	uint16_t systemVersion;
@@ -44,6 +54,53 @@ typedef struct MCP39F521_FormattedData {
 	float apparentPower;
 } MCP39F521_FormattedData;
 
+int readMCP32f521(int addressHigh, int addressLow, int numBytesToRead, uint8_t *byteArray, int byteArraySize)
+{
+  constexpr size_t I2C_BUFFER_SIZE = 32;
+  uint8_t I2C_ADDRESS = 0x74;
+  uint32_t checksumTotal = 0;
+  uint8_t i2c_bus_Status = 0;
+  uint8_t ReadDataBuf[8];
+  int i;
+  ReadDataBuf[0] = 0xA5; // Header
+  ReadDataBuf[1] = 0x08; // Num bytes
+  ReadDataBuf[2] = 0x41; // Command - set address pointer
+  ReadDataBuf[3] = addressHigh;
+  ReadDataBuf[4] = addressLow;
+  ReadDataBuf[5] = 0x4E; // Command - read register, N bytes
+  ReadDataBuf[6] = 0x20; 
+  ReadDataBuf[7] = 0x5e; // Checksum 0x05E - computed below
+  for(i = 0; i < 7; i++) {
+    checksumTotal += ReadDataBuf[i];
+  }
+  ReadDataBuf[7] = checksumTotal % 256; // 0x5E = 94 
+  Serial.print("Checksum = "); Serial.println(ReadDataBuf[7]);
+  Wire.beginTransmission(I2C_ADDRESS);
+  for(i= 0; i < 8; i++) {
+    Wire.write(ReadDataBuf[i]);
+  }
+  i2c_bus_Status = Wire.endTransmission(true);
+  wireErrors(i2c_bus_Status);
+  delay(5);
+
+  Wire.requestFrom(I2C_ADDRESS, (uint8_t)32); // request the bytes
+  size_t bytes_read = Wire.requestFrom(I2C_ADDRESS, I2C_BUFFER_SIZE);
+  //uint8_t baseline[I2C_BUFFER_SIZE];
+  if ( bytes_read == I2C_BUFFER_SIZE ) {
+    int requestDataLength = Wire.available();
+    if (requestDataLength==(numBytesToRead + 3)) {
+      for (i = 0; i < requestDataLength ; i++) {
+        byteArray[requestDataLength - i] = Wire.read();
+        Serial.print(byteArray[requestDataLength - i], HEX); Serial.print(" ");
+      }
+    }else {
+    // Unexpected. Handle error  
+    return 5; 
+  }
+  }
+  return 0;
+}
+
 void wireErrors(uint8_t i2c_bus_Status){
   if(i2c_bus_Status == 0){
     Serial.print("I2C bus Status Success = "); Serial.println(i2c_bus_Status);
@@ -58,6 +115,49 @@ void wireErrors(uint8_t i2c_bus_Status){
   }else if(i2c_bus_Status == 5){
     Serial.print("Data byte transfer succeeded, busy timeout immediately after = "); Serial.println(i2c_bus_Status);
   }
+}
+
+int tinkerDigitalWrite(String command)
+{
+    bool value = 0;
+    //convert ascii to integer
+    int pinNumber = command.charAt(1) - '0';
+    //Sanity check to see if the pin numbers are within limits
+    if (pinNumber < 0 || pinNumber > 7) return -1;
+
+    if(command.substring(3,7) == "HIGH") value = 1;
+    else if(command.substring(3,6) == "LOW") value = 0;
+    else return -2;
+
+    if(command.startsWith("D"))
+    {
+        pinMode(pinNumber, OUTPUT);
+        digitalWrite(pinNumber, value);
+        return 1;
+    }
+    else if(command.startsWith("A"))
+    {
+        pinMode(pinNumber+10, OUTPUT);
+        digitalWrite(pinNumber+10, value);
+        return 1;
+    }
+#if Wiring_Cellular
+    else if(command.startsWith("B"))
+    {
+        if (pinNumber > 5) return -4;
+        pinMode(pinNumber+24, OUTPUT);
+        digitalWrite(pinNumber+24, value);
+        return 1;
+    }
+    else if(command.startsWith("C"))
+    {
+        if (pinNumber > 5) return -5;
+        pinMode(pinNumber+30, OUTPUT);
+        digitalWrite(pinNumber+30, value);
+        return 1;
+    }
+#endif
+    else return -3;
 }
 
 void convertdata(MCP39F521_Data *data, MCP39F521_FormattedData *fData)
@@ -94,62 +194,43 @@ void printMCP39F521Data(MCP39F521_FormattedData *data)
   Serial.print(F("Apparent Power = ")); Serial.println(data->apparentPower, 4);
 }
 
-constexpr size_t I2C_BUFFER_SIZE = 32;
-uint8_t I2C_ADDRESS = 0x74;
-uint8_t numBytesToRead = 28;
-uint8_t ReadDataBuf[8];
-uint8_t byteArray[32];
-uint32_t checksumTotal = 0;
-uint8_t i2c_bus_Status = 0;
-int i;
+void LM75A_TEMP_READING()
+{
+  float temperature_in_degrees = lm75a_sensor.getTemperatureInDegrees();
+
+  if (temperature_in_degrees == INVALID_LM75A_TEMPERATURE) {
+    Serial.println("Error while getting temperature");
+  } else {
+    Serial.print("Temperature: ");
+    Serial.print(temperature_in_degrees);
+    Serial.print(" degrees (");
+    Serial.print(LM75A::degreesToFahrenheit(temperature_in_degrees));
+    Serial.println(" fahrenheit)");
+  }
+}
+
 
 void setup() {
-    WiFi.off();
-    Serial.begin(115200);
-    Wire.setSpeed(CLOCK_SPEED_100KHZ);
-    //Wire.stretchClock(true);
-    Wire.begin();
+  WiFi.off();
+  Serial.begin(115200);
+  pinMode(D7, OUTPUT);
+  digitalWrite(D7, HIGH);
+  Wire.setSpeed(CLOCK_SPEED_100KHZ);
+  //Wire.stretchClock(true);
+  Wire.begin();
+  Particle.function("digitalwrite", tinkerDigitalWrite);
+  if (Particle.connected() == false) {
+    Particle.connect();
+  }
 }
 
 void loop() 
 { 
   MCP39F521_Data data;
   MCP39F521_FormattedData fData;
-  ReadDataBuf[0] = 0xA5; // Header
-  ReadDataBuf[1] = 0x08; // Num bytes
-  ReadDataBuf[2] = 0x41; // Command - set address pointer
-  ReadDataBuf[3] = 0x00;
-  ReadDataBuf[4] = 0x02;
-  ReadDataBuf[5] = 0x4E; // Command - read register, N bytes
-  ReadDataBuf[6] = 0x20; 
-  ReadDataBuf[7] = 0x5e; // Checksum 0x05E - computed below
-  for(i = 0; i < 7; i++) {
-    checksumTotal += ReadDataBuf[i];
-  }
-  ReadDataBuf[7] = checksumTotal % 256; // 0x5E = 94 
-  Serial.print("Checksum = "); Serial.println(ReadDataBuf[7]);
-  Wire.beginTransmission(I2C_ADDRESS);
-  for(i= 0; i < 8; i++) {
-    Wire.write(ReadDataBuf[i]);
-  }
-  i2c_bus_Status = Wire.endTransmission(true);
-  wireErrors(i2c_bus_Status);
-  delay(5);
-
-  Wire.requestFrom(I2C_ADDRESS, (uint8_t)32); // request the bytes
-  size_t bytes_read = Wire.requestFrom(I2C_ADDRESS, I2C_BUFFER_SIZE);
-  //uint8_t baseline[I2C_BUFFER_SIZE];
-  if ( bytes_read == I2C_BUFFER_SIZE ) {
-    int requestDataLength = Wire.available();
-    // if (requestDataLength==(numBytesToRead + 3)) {
-      for (i = 0; i < requestDataLength ; i++) {
-        byteArray[requestDataLength - i] = Wire.read();
-        Serial.print(byteArray[requestDataLength - i], HEX); Serial.print(" ");
-      }
-    // }
-  }
-
-  Serial.print("\n");
+  uint8_t byteArray[35];
+  int reVal = readMCP32f521(0x00, 0x02, 28, byteArray, 35);
+  Serial.print("reVal:"); Serial.println(reVal); 
   data.systemStatus = ((byteArray[3] << 8) | byteArray[2]);
   data.systemVersion = ((byteArray[5] << 8) | byteArray[4]);
   data.voltageRMS = ((byteArray[7] << 8) | byteArray[6]);
@@ -175,5 +256,6 @@ void loop()
                             byteArray[26]);
   convertdata(&data, &fData);
   printMCP39F521Data(&fData);
+  //LM75A_TEMP_READING();
   delay(1000);
 }
